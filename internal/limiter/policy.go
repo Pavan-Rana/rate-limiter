12310328fd113ce3fca4f4f9676caaa9223867d4
psrbr157@gmail.com <psrbr@PavansLaptop.>
2026-03-28 23:12:18 +0000

    Add policy and default policy

--- internal/limiter/policy.go
+++ internal/limiter/policy.go
@@ -1,1 +1,14 @@
-// internal/limiter/policy.go
+package limiter
+
+import "time"
+
+type Policy struct {
+	Limit	 	int
+	Window	 	time.Duration
+	BurstLimit 	int
+}
+
+var DefaultPolicy = Policy{
+	Limit: 100,
+	Window: time.Minute
+}
